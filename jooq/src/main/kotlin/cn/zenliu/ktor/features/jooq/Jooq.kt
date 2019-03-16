59dddde0bf4a9b84e9677eb6a76fdd582a91b71c
Zen.Liu <lcz20@163.com>
2019-03-16 20:28:29 +0800

    [jooq] add extra datasource support

--- jooq/src/main/kotlin/cn/zenliu/ktor/features/jooq/Jooq.kt
+++ jooq/src/main/kotlin/cn/zenliu/ktor/features/jooq/Jooq.kt
@@ -6,6 +6,7 @@ import cn.zenliu.ktor.features.template.FeatureTemplate
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.ktor.application.Application
+import org.jooq.DSLContext
 import org.jooq.SQLDialect
 import org.jooq.conf.Settings
 import org.jooq.exception.ConfigurationException
@@ -66,7 +67,17 @@ class Jooq {
                 SQLDialect.valueOf(config!!.dialect)
             )
         }
-
+        private val extra: MutableMap<String, DSLContext> = mutableMapOf()
+        /**
+         * fetch DslContext defined in [JooqConf.extra]
+         * @param name String
+         * @return DSLContext?
+         */
+        fun extra(name: String) = extra[name] ?: config!!.extra[name]?.let {
+            createDSLWithPool(it).apply {
+                extra.put(name, this)
+            }
+        }
         /**
          * create DSL from parameter
          * @param url String
@@ -77,7 +88,7 @@ class Jooq {
          * @return [org.jooq.DSLContext]
          */
         @JvmStatic
-        fun createDSL(url: String, user: String, password: String, driver: String, dialect: SQLDialect) = {
+        fun createDSLOutPool(url: String, user: String, password: String, driver: String, dialect: SQLDialect) = {
             Class.forName(driver)
             DSL.using(
                 DriverManager.getConnection(url, user, password), dialect,
@@ -85,6 +96,44 @@ class Jooq {
             )
         }.invoke()
 
+        /**
+         * create extra DslContext with HikariCP
+         * @param url String
+         * @param user String
+         * @param password String
+         * @param driver String
+         * @param dialect SQLDialect
+         * @return DSLContext
+         */
+        @JvmStatic
+        fun createDSLWithPool(source: Datasource) = DSL.using(
+            HikariDataSource(HikariConfig().apply {
+                jdbcUrl = source.url
+                if (source.username.isNotBlank()) {
+                    username = source.username
+                }
+
+                if (source.password.isNotBlank()) {
+                    password = source.password
+                }
+
+                maximumPoolSize = source.poolSize ?: this@Feature.config!!.poolSize
+                addDataSourceProperty(
+                    "cachePrepStmts",
+                    source.cachePrepStmts ?: this@Feature.config!!.cachePrepStmts
+                )
+                addDataSourceProperty(
+                    "prepStmtCacheSize",
+                    source.prepStmtCacheSize ?: this@Feature.config!!.prepStmtCacheSize
+                )
+                addDataSourceProperty(
+                    "prepStmtCacheSqlLimit",
+                    source.prepStmtCacheSqlLimit ?: this@Feature.config!!.prepStmtCacheSqlLimit
+                )
+            }),
+            SQLDialect.valueOf(source.dialect ?: this@Feature.config!!.dialect),
+            settings
+        )!!
         /**
          * create DSL from other datasource
          * @param ds DataSource
@@ -106,9 +155,20 @@ class Jooq {
             var poolSize: Int = 10,
             var cachePrepStmts: Boolean = true,
             var prepStmtCacheSize: Int = 250,
-            var prepStmtCacheSqlLimit: Int = 2048
+            var prepStmtCacheSqlLimit: Int = 2048,
+            var extra: MutableMap<String, Datasource> = mutableMapOf()
         )
 
+        class Datasource(
+            var url: String,
+            var username: String = "",
+            var password: String = "",
+            var dialect: String? = null,
+            var poolSize: Int? = null,
+            var cachePrepStmts: Boolean? = null,
+            var prepStmtCacheSize: Int? = null,
+            var prepStmtCacheSqlLimit: Int? = null
+        )
 
     }
 }
